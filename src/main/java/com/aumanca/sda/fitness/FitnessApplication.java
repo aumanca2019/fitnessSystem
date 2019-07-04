38a10703a7ed39ecc79289c2da79ddec56261ba6
Laura81 <laura.iordache@arcadeproperty.ro>
2019-07-04 20:56:39 +0300

    update git ignore

--- src/main/java/com/aumanca/sda/fitness/FitnessApplication.java
+++ src/main/java/com/aumanca/sda/fitness/FitnessApplication.java
@@ -2,13 +2,23 @@ package com.aumanca.sda.fitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FitnessApplication {
+public class FitnessApplication implements WebMvcConfigurer {
+
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(FitnessApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/resources/**")
+				.addResourceLocations("/resources/");
 	}
 
 }
--- src/main/java/com/aumanca/sda/fitness/controller/UserController.java
+++ src/main/java/com/aumanca/sda/fitness/controller/UserController.java
@@ -1,47 +1,28 @@
 package com.aumanca.sda.fitness.controller;
 
 import com.aumanca.sda.fitness.dto.UserRequest;
-import com.aumanca.sda.fitness.dto.UserResponse;
-import com.aumanca.sda.fitness.model.User;
 import com.aumanca.sda.fitness.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-
-import java.util.List;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("api/users")
+@RequestMapping("/register")
 public class UserController {
 
-    @Autowired
-    public UserService userService;
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+
+    }
 
     @PostMapping
     public String createUser(@ModelAttribute UserRequest userRequest) {
         userService.save(userRequest);
-        return "index";
-    }
-    @GetMapping
-    public ResponseEntity<List<User>> getAll(){
-        List<User> users = userService.findAll();
-
-        if(users.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
-    @GetMapping("/{id}")
-    public UserResponse getById(@PathVariable(required = true) Long id) {
-        return userService.getUserById(id);
+        return  "index";
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable("id") final Long id) {
-        userService.delete(id);
-
-    }
 }
